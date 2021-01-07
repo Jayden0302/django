1aee10a09b56f5676ba529e735a1066ab7e6e310
jaising <js.tan@skybox.com.my>
2021-01-08 02:58:23 +0800

    first commit

---
+++ CMSProject/mysite/mysite/search_indexes.py
@@ -0,0 +1,15 @@
+import datetime
+from haystack import indexes
+from cms.models import Page
+
+class PageIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='user')
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return Page
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
